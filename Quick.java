2f8507134b592dfea9202cb73580ee7dbc36a7ad
Michael Lin <mlin01@stuy.edu>
2019-03-12 10:17:43 -0400

    improving partition

--- Quick.java
+++ Quick.java
@@ -2,39 +2,65 @@ import java.util.Random;
 import java.util.Arrays;
 
 public class Quick{
+
+  private static void swap(int[] data, int a, int b){
+    int temp = data[a];
+    data[a] = data[b];
+    data[b] = temp;
+  }
+
   public static int partition(int[] data, int start, int end){
     if(start==end) return start;
     Random r = new Random();
     int pivot = start+Math.abs(r.nextInt())%(end-start);
     //System.out.println(data[pivot]);
-    int temp = data[start];
-    data[start]=data[pivot];
-    data[pivot]=temp;
+    swap(data,start,pivot);
     pivot=start;
     start++;
     while(start!=end){
       if(data[start]>data[pivot]){ //if value is greater than pivot
-        int t = data[start];
-        data[start]=data[end];
-        data[end]=t;
+        swap(data,start,end);
         end--;
       }
       else{
         start++;
       }
     }
-    if(data[end]>data[pivot]){
-      int t = data[end-1];
-      data[end-1]=data[pivot];
-      data[pivot]=t;
-      pivot=end-1;
-    }
+    if(data[end]>data[pivot]) end--;
+    swap(data,pivot,end);
+    pivot = end;
+
+    return pivot;
+  }
+
+  public static int partitionImp(int[] data, int start, int end){
+    if(start==end) return start;
+    Random r = new Random();
+    int middle = (start+end)/2;
+    int pivot = start;
+    int med = Math.max(Math.min(data[start],data[middle]) , Math.min(Math.max(data[start],data[middle]), data[end])); //max of the 2 smallest
+    if (med == data[start]) pivot = data[start];
     else{
-      int t = data[end];
-      data[end]=data[pivot];
-      data[pivot]=t;
-      pivot=end;
+      if(med == data[end]) pivot = [end];
+      else pivot = data[middle];
     }
+    //System.out.println(data[pivot]);
+    swap(data,start,pivot);
+    pivot=start;
+    start++;
+    while(start!=end){
+      if(data[start]>data[pivot]){ //if value is greater than pivot
+        swap(data,start,end);
+        end--;
+      }
+      else{
+        start++;
+      }
+    }
+    if(data[end]>data[pivot]) end--;
+    swap(data,pivot,end);
+    pivot = end;
+
     return pivot;
   }
 
